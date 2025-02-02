96a5cb30ded6889263e0714be3433e1197c1a7e7
Donia Alhosin <doniaelhussien@gmail.com>
2025-02-02 09:52:47 +0200

    Applying Not FoundPage

--- src/app/page/not-found-page/not-found-page.component.ts
+++ src/app/page/not-found-page/not-found-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.css'
 })
 export class NotFoundPageComponent {
+  faHome=faHome
 
 }
